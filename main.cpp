e2e9a27e53a72c3a876a05c0f67e199476245eef
jacobneff <jacobleeneff@gmail.com>
2023-09-29 15:08:57 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string passage =
+      "If I am walking with two other men, each of them will serve as my "
+      "teacher. I will pick out the good points of the one and imitate them, "
+      "and the bad points of the other and correct them in myself.";
+
+  std::string test;
+
+  std::cout << passage << std::endl;
+  std::cin >> test;
+
+  return 0;
+}
