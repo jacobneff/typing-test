d3765058bf8fc95faffd49d5d6995f9e46fa97fc
jacobneff <jacobleeneff@gmail.com>
2023-10-03 17:37:42 -0400

    added wpm calculation

--- main.cpp
+++ main.cpp
@@ -1,49 +1,59 @@
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <chrono>
 #include <unistd.h>
 
-std::string typingTest(std::string, std::chrono::duration<double>&);
-void results(std::chrono::duration<double>);
+std::string typingTest(std::string, std::chrono::duration<double> &);
+void results(std::chrono::duration<double>, std::string);
 void countdown();
 
 int main() {
   using namespace std::chrono;
-  
+
   std::string passage =
       "If I am walking with two other men, each of them will serve as my "
       "teacher. I will pick out the good points of the one and imitate them, "
       "and the bad points of the other and correct them in myself.";
 
   duration<double> time_span;
-  
+
   countdown();
   std::string input = typingTest(passage, time_span);
-  results(time_span);
+  results(time_span, input);
 
   return 0;
 }
 
-std::string typingTest(std::string passage, std::chrono::duration<double>& time_span) {
+std::string typingTest(std::string passage,
+                       std::chrono::duration<double> &time_span) {
   using namespace std::chrono;
   std::string input;
-  
+
   steady_clock::time_point t1 = steady_clock::now();
 
   std::cout << passage << std::endl << std::endl;
   std::getline(std::cin, input);
 
   steady_clock::time_point t2 = steady_clock::now();
-  time_span = duration_cast<duration<double>>(t2-t1);
+  time_span = duration_cast<duration<double>>(t2 - t1);
 
   return input;
 }
 
-void results(std::chrono::duration<double> time_span) {
+void results(std::chrono::duration<double> time_span, std::string input) {
+  float time_seconds, wpm, allTypedEntries;
+
+  time_seconds = time_span.count();
+
+  allTypedEntries = input.length();
+
+  wpm = (allTypedEntries / 5) / (time_seconds / 60);
+
   std::cout << std::endl;
-  
-  std::cout << "It took you " << time_span.count() << " seconds to type the passage.";
+  std::cout << "It took you " << time_seconds << " seconds to type the passage."
+            << std::endl;
+  std::cout << "Your typing speed was " << wpm << " wpm";
   std::cout << std::endl;
 }
 
