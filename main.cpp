e1c52ec5eb4404503346c90cb22fb1a6bf17cb97
jacobneff <jacobleeneff@gmail.com>
2023-10-03 17:08:01 -0400

    added a countdown and split the current functionality into separate functions

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <unistd.h>
+
+std::string typingTest(std::string, std::chrono::duration<double>&);
+void results(std::chrono::duration<double>);
+void countdown();
 
 int main() {
   using namespace std::chrono;
@@ -11,18 +16,42 @@ int main() {
       "teacher. I will pick out the good points of the one and imitate them, "
       "and the bad points of the other and correct them in myself.";
 
-  std::string test;
+  duration<double> time_span;
+  
+  countdown();
+  std::string input = typingTest(passage, time_span);
+  results(time_span);
+
+  return 0;
+}
+
+std::string typingTest(std::string passage, std::chrono::duration<double>& time_span) {
+  using namespace std::chrono;
+  std::string input;
   
   steady_clock::time_point t1 = steady_clock::now();
 
   std::cout << passage << std::endl << std::endl;
-  std::cin >> test;
+  std::getline(std::cin, input);
 
   steady_clock::time_point t2 = steady_clock::now();
-  duration<double> time_span = duration_cast<duration<double>>(t2-t1);
+  time_span = duration_cast<duration<double>>(t2-t1);
 
+  return input;
+}
+
+void results(std::chrono::duration<double> time_span) {
+  std::cout << std::endl;
+  
   std::cout << "It took you " << time_span.count() << " seconds to type the passage.";
   std::cout << std::endl;
+}
 
-  return 0;
+void countdown() {
+  for (int i = 3; i > 0; i--) {
+    std::cout << "Begin typing in " << i << std::endl;
+    sleep(1);
+  }
+
+  std::cout << std::endl << std::endl;
 }
